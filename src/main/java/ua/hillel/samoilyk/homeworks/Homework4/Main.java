a77a93e39622e7063f8e7e349ba3db7c94c55291
Denis Samoilyk <deniskakbsamoilyk@gmail.com>
2024-02-19 19:16:01 +0200

    Homework4

---
+++ src/main/java/ua/hillel/samoilyk/homeworks/Homework4/Main.java
@@ -0,0 +1,17 @@
+package ua.hillel.samoilyk.homeworks.Homework4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 4;
+        int c = 6;
+
+        int volume = a * b * c;
+        int length = a + b + c;
+
+        System.out.println("Об'єм паралелепіпеда: " + volume);
+        System.out.println("Сумарна довжина всіх сторін: " + length);
+    }
+
+}
