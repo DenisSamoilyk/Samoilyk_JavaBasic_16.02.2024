21d2a50c8d7b7d5a8de681d9d6b58b74977ceb7a
Denis Samoilyk <deniskakbsamoilyk@gmail.com>
2024-02-27 19:50:23 +0200

    Homework6 rework

--- src/main/java/ua/hillel/samoilyk/homeworks/Homework6/Main.java
+++ src/main/java/ua/hillel/samoilyk/homeworks/Homework6/Main.java
@@ -10,26 +10,34 @@ public class Main {
         System.out.println("Write first Team: ");
         String firstTeam = scanner.nextLine();
 
-        int player1Team1, player2Team1, player3Team1, player4Team1, player5Team1;
+        int player1Team1 = 0;
+        int player2Team1 = 0;
+        int player3Team1 = 0;
+        int player4Team1 = 0;
+        int player5Team1 = 0;
 
         System.out.println("How much kills player 1 have in team: " + '"' + firstTeam + '"');
         if (scanner.hasNextInt()) {
             player1Team1 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 2 have in team: " + '"' + firstTeam + '"');
+        System.out.println("How much kills player 2 have in team: " + '"' + firstTeam + '"');
+        } if (scanner.hasNextInt()){
             player2Team1 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 3 have in team: " + '"' + firstTeam + '"');
+        System.out.println("How much kills player 3 have in team: " + '"' + firstTeam + '"');
+        } if (scanner.hasNextInt()){
             player3Team1 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 4 have in team: " + '"' + firstTeam + '"');
+        System.out.println("How much kills player 4 have in team: " + '"' + firstTeam + '"');
+        } if (scanner.hasNextInt()){
             player4Team1 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 5 have in team: " + '"' + firstTeam + '"');
+        System.out.println("How much kills player 5 have in team: " + '"' + firstTeam + '"');
+        } if (scanner.hasNextInt()){
             player5Team1 = scanner.nextInt();
             scanner.nextLine();
         } else {
@@ -41,46 +49,58 @@ public class Main {
         System.out.println("Write second Team: ");
         String secondTeam = scanner.nextLine();
 
-        int player1Team2, player2Team2, player3Team2, player4Team2, player5Team2;
+        int player1Team2 = 0;
+        int player2Team2 = 0;
+        int player3Team2 = 0;
+        int player4Team2 = 0;
+        int player5Team2 = 0;
 
         System.out.println("How much kills player 1 have in team: " + '"' + secondTeam + '"');
         if (scanner.hasNextInt()) {
             player1Team2 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 2 have in team: " + '"' + secondTeam + '"');
+        System.out.println("How much kills player 2 have in team: " + '"' + secondTeam + '"');
+        } if (scanner.hasNextInt()){
             player2Team2 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 3 have in team: " + '"' + secondTeam + '"');
+        System.out.println("How much kills player 3 have in team: " + '"' + secondTeam + '"');
+        } if (scanner.hasNextInt()){
             player3Team2 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 4 have in team: " + '"' + secondTeam + '"');
+        System.out.println("How much kills player 4 have in team: " + '"' + secondTeam + '"');
+        } if (scanner.hasNextInt()){
             player4Team2 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("How much kills player 5 have in team: " + '"' + secondTeam + '"');
+        System.out.println("How much kills player 5 have in team: " + '"' + secondTeam + '"');
+        } if (scanner.hasNextInt()){
             player5Team2 = scanner.nextInt();
             scanner.nextLine();
+
         } else {
             System.out.println("Error");
             return;
         }
 
         // First team average kills
-        int sum = (player1Team1 + player2Team1 + player3Team1 + player4Team1 + player5Team1) / 5;
+        float sum = (player1Team1 + player2Team1 + player3Team1 + player4Team1 + player5Team1) / 5;
         System.out.println("Team: " + '"' + firstTeam + '"' + " has average " + sum + " kills");
 
         // Second team average kills
-        int sum2 = (player1Team2 + player2Team2 + player3Team2 + player4Team2 + player5Team2) / 5;
+        float sum2 = (player1Team2 + player2Team2 + player3Team2 + player4Team2 + player5Team2) / 5;
         System.out.println("Team: " + '"' + secondTeam + '"' + " has average " + sum2 + " kills");
 
         // Who win?
         if (sum > sum2) {
             System.out.println("Team: " + '"' + firstTeam + '"' + " WIN!");
-        } else {
+        } else if (sum < sum2){
             System.out.println("Team: " + '"' + secondTeam + '"' + " WIN!");
+        } else {
+            System.out.println("Team: " + '"' + firstTeam + '"' + " and "
+                    + "Team: " + '"' + secondTeam + '"' + " has draw!");
         }
     }
 }
