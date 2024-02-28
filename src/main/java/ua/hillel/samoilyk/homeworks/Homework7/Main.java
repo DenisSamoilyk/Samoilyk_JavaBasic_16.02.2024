c929af9e5810ed8224d48af7aac30283bb926e2d
Denis Samoilyk <deniskakbsamoilyk@gmail.com>
2024-02-28 17:31:33 +0200

    Homework7 fix

--- src/main/java/ua/hillel/samoilyk/homeworks/Homework7/Main.java
+++ src/main/java/ua/hillel/samoilyk/homeworks/Homework7/Main.java
@@ -9,26 +9,33 @@ public class Main {
         int attempts = 0;
 
         int randomNumber = (int) (Math.random()*11);
-        System.out.println("Computer number:" + randomNumber);
+        System.out.println("Computer number: " + randomNumber);
 
         while (attempts < 3) {
+
             System.out.println("Guess the number from 0 to 10");
-            int userNumber = scanner.nextInt();
-            scanner.nextLine();
-            attempts++;
+            if (scanner.hasNextInt()) {
+                int userNumber = scanner.nextInt();
+                scanner.nextLine();
+                attempts++;
 
-            if (userNumber == randomNumber) {
-                System.out.println("You WIN!");
-                break;
-            } else if (userNumber > randomNumber) {
-                System.out.println("Your number is higher than the guess number");
-            } else {
-                System.out.println("Your number is smaller than the guess number");
-
-                if (attempts == 3) {
-                    System.out.println("Sorry, you've run out of attempts. The correct number was: " + randomNumber);
+                if (userNumber == randomNumber) {
+                    System.out.println("You WIN!");
                     break;
+                } else if (userNumber > randomNumber) {
+                    System.out.println("Your number is higher than the guess number");
+                } else {
+                    System.out.println("Your number is smaller than the guess number");
+
+                    if (attempts == 3) {
+                        System.out.println("Sorry, you've run out of attempts. The correct number was: " + randomNumber);
+                        break;
+                    }
                 }
+
+            } else {
+                System.out.println("Error. You need write digit!");
+                break;
             }
         }
     }
